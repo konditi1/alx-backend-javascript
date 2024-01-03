15cc87fd10e4cf6f55e2ada767de133f62479d37
konditi1 <konditi1@gmail.com>
2024-01-03 15:29:09 +0300

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,6 @@
-export default function taskBlock(trueOrFalse) {
-  const task = true;
-  const task2 = false;
-
-  if (trueOrFalse) {
-    let  task = true;
-    let  task2 = false;
-  }
+export default function taskBlock() {
+  const task = false;
+  const task2 = true;
 
   return [task, task2];
 }
