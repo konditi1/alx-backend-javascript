b59c98079af259053389928ee48d2aeefe792c6f
konditi1 <konditi1@gmail.com>
2024-01-03 20:02:49 +0300

    for loop

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const modifiedArray = [];
+  for (const item of array) {
+    modifiedArray.push(appendString + item);
+  }
+
+  return modifiedArray;
+}
