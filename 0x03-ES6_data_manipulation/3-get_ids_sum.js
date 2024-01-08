52b7e60d0bee2982c7172380971053de19aeac79
konditi1 <konditi1@gmail.com>
2024-01-08 23:58:20 +0300

    reduce

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,8 @@
+function getStudentIdsSum(students) {
+  if (!Array.isArray(students)) {
+    return 0;
+  }
+  return students.reduce((acc, student) => acc + student.id, 0);
+}
+
+export default getStudentIdsSum;
