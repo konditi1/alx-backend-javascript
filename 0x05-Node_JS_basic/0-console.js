200629152dc19430c96906f254818304b380b7cf
konditi1 <konditi1@gmail.com>
2024-02-26 20:15:17 +0300

    displays str arguments

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays the message to the console.
+ * @param {string} str - The message to be displayed.
+ */
+function displayMessage(str) {
+    console.log(str);
+}
+
+module.exports = displayMessage
