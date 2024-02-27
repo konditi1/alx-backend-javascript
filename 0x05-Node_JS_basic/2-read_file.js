64885995c6b93e1285050e97ff76b83950a5ae01
konditi1 <konditi1@gmail.com>
2024-02-27 21:06:40 +0300

    read file

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+/**
+ * Counts the number of students and organizes them by field.
+ * @param {string} path - The path to the database file.
+ * @throws {Error} Throws an error if the database file cannot be loaded.
+ */
+function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+
+  const data = fs.readFileSync(path, 'utf8');
+  const lines = data.split('\n');
+  const hashtable = {};
+  let students = -1;
+
+  for (const line of lines) {
+    if (line.trim() !== '') {
+      const columns = line.split(',');
+      const field = columns[3];
+      const firstname = columns[0];
+
+      if (students >= 0) {
+        if (!Object.hasOwnProperty.call(hashtable, field)) {
+          hashtable[field] = [];
+        }
+        hashtable[field] = [...hashtable[field], firstname];
+      }
+      students += 1;
+    }
+  }
+
+  console.log(`Number of students: ${students}`);
+
+  for (const key in hashtable) {
+    if (Object.hasOwnProperty.call(hashtable, key)) {
+      console.log(`Number of students in ${key}: ${hashtable[key].length}. List: ${hashtable[key].join(', ')}`);
+    }
+  }
+}
+
+module.exports = countStudents;
